0e08bca76899750613e3c0aa712b2ffe6a931a67
kalyandasari14 <kalyandasari80@gmail.com>
2026-03-27 15:37:24 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by kalyan on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
